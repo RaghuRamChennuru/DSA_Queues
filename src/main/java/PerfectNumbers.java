649907843692607a639e71691ae5cd01f3dc6c86
unknown <raghuram6857@gmail.com>
2024-06-01 20:43:53 +0530

    Queues

---
+++ src/main/java/PerfectNumbers.java
@@ -0,0 +1,18 @@
+/*
+* Given an integer A, you have to find the Ath Perfect Number.
+
+A Perfect Number has the following properties:
+
+It comprises only 1 and 2.
+The number of digits in a Perfect number is even.
+It is a palindrome number.
+For example, 11, 22, 112211 are Perfect numbers, where 123, 121, 782, 1 are not.
+* */
+
+public class PerfectNumbers
+{
+    public String solve(int A)
+    {
+
+    }
+}
